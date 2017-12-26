7c57c21ba3102c9dcc50710fc0035e284e2c5aff
jamesthall90 <jamesthall90@gmail.com>
2017-12-26 11:34:36 -0500

    Initial Commit

---
+++ Island HopperTests/Island_HopperTests.swift
@@ -0,0 +1,35 @@
+//
+//  Island_HopperTests.swift
+//  Island HopperTests
+//
+//  Created by James Hall on 12/26/17.
+//
+
+import XCTest
+@testable import Island_Hopper
+
+class Island_HopperTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
